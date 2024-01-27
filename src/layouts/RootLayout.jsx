6c0d058fbaf3cfee6c73a9081d709a1b3e7edf8d
Abass Koyang <abasskoyang12345@gmail.com>
2024-01-28 04:32:30 +0930

    add: header and sidebar components

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+import SideBar from "../components/SideBar"
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <main className="">
+        <Header />
+        <SideBar />
+        <Outlet />
+    </main>
   )
 }
 
