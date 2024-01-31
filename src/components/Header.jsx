bffb140246c1f0a11ba638197d395bae83394917
Abass Koyang <abasskoyang12345@gmail.com>
2024-01-31 16:59:36 +0930

    styled header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center justify-center">
+    <header className="w-full flex items-center justify-center py-4 xl:py-[18px] bg-[#FAFAFA]">
       <div className="w-full max-w-[1400px] flex justify-between items-center">
-        <h1>Header</h1>
-        <nav className="flex items-center gap-3">
+        <h1 className="text-[20px] leading-7 font-semibold">Dashboard</h1>
+        <div className="flex items-center gap-3">
           <li>link</li>
           <li>link</li>
           <li>link</li>
           <li>link</li>
-        </nav>
+        </div>
       </div>
     </header>
   )
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -6,7 +6,7 @@ const RootLayout = () => {
   return (
     <main className="w-full bg-green-400 h-dvh transition-all duration-200 ease-in-out">
         <SideBar />
-        <section className='pl-[80px] p-10 bg-red-300 w-full'>
+        <section className='p-0 m-0 pl-[53.9px] xl:pl-[63px] bg-red-300'>
         <Header />
         <Outlet />
         </section>
