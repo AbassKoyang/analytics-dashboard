fdb498f306797c6ac7c798c1caf74da9478e6a62
Abass Koyang <abasskoyang12345@gmail.com>
2024-01-31 10:04:49 +0930

    completed sidebar: added settings, arrow and logout buttons. Made sidebar responsive on smalll laptop screens and large screens

--- src/components/DarkModeToggleButton.jsx
+++ src/components/DarkModeToggleButton.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
-import { SunIcon } from "../assets/icons";
+import { MoonIcon, SunIcon } from "../assets/icons";
 
 const DarkModeToggleButton = () => {
-    const [localStorageState, setLocalStorageState] = useState({});
     const [theme, setTheme] = useState('dark');
     const [activeTheme, setActiveTheme] = useState('system');
   
     
   // Get local storage theme,  if it is 'dark' add the dark class to the documentElement classList.
     useEffect(() => {
-      setLocalStorageState(localStorage);
       if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark')
       } else {
@@ -22,35 +20,33 @@ const DarkModeToggleButton = () => {
     useEffect(() => {
       if (localStorage.theme === 'dark') {
         setActiveTheme('dark');
-      } else if(localStorage.theme === 'light') {
+      } else {
         setActiveTheme('light');
-      } else if (localStorage.theme === 'system') {
-        setActiveTheme('system');
-      }
+        }
     }, [activeTheme]);
   
     //set active theme to light and also update localstorage
     const setLight = () => {
-      localStorageState.theme = 'light';
+      localStorage.theme = 'light';
       setTheme('light');
       setActiveTheme('light');
     }
   
     //set active theme to dark and also update localstorage
     const setDark = () => {
-      localStorageState.setItem('theme', 'dark')
-      localStorageState.theme = 'dark';
+      localStorage.setItem('theme', 'dark')
+      localStorage.theme = 'dark';
       setTheme('dark');
       setActiveTheme('dark');
     };
 
   return (
-    <div role='radiogroup' className="flex gap-0 rounded-full p-2 bg-white">
-        <button aria-checked={`${activeTheme === 'dark'? 'true' : 'false'}`} aria-label='Switch to dark theme' role='radio' onClick={setDark} className={`group rounded-full p-2  ${activeTheme === 'dark'? 'bg-[#E6E6E6] dark:bg-[#292929]' : ''}`}>
-        <RiMoonLine className={`w-3 h-3 group-hover:text-black group-hover:dark:text-white ${activeTheme === 'dark'? 'text-black dark:text-white' : 'text-black/50 dark:text-white/50'}`} />
+    <div role='radiogroup' className="flex flex-col gap-2 rounded-full p-1.5 bg-white  transition-all duration-200 ease-in-out">
+        <button aria-checked={`${activeTheme === 'dark'? 'true' : 'false'}`} aria-label='Switch to dark theme' role='radio' onClick={setDark} className={`group rounded-full p-1 ${activeTheme === 'dark'? 'bg-[#34CAA5] dark:bg-[#34CAA5]' : ''} transition-all duration-200 ease-in-out`}>
+        <MoonIcon className={`w-5 h-5 ${activeTheme === 'dark'? 'fill-white dark:fill-white' : 'fill-[#B2ABAB] dark:fill-white/50 group-hover:fill-[#949191] group-hover:dark:fill-[#dad5d5]'} transition-all duration-200 ease-in-out`} />
         </button>
-        <button aria-checked={`${activeTheme === 'light'? 'true' : 'false'}`} aria-label='Switch to light theme' role='radio' onClick={setLight} className={`group rounded-full p-2 ${activeTheme === 'light'? 'bg-[#34CAA5] dark:bg-[#34CAA5]' : ''}`}>
-        <SunIcon className={`w-3 h-3 group-hover:fill-white group-hover:dark:fill-white ${activeTheme === 'light'? 'fill-black dark:fill-white' : 'fill-[#B2ABAB] dark:fill-white/50'}`} />
+        <button aria-checked={`${activeTheme === 'light'? 'true' : 'false'}`} aria-label='Switch to light theme' role='radio' onClick={setLight} className={`group rounded-full p-1 ${activeTheme === 'light'? 'bg-[#34CAA5] dark:bg-[#34CAA5]' : ''} transition-all duration-200 ease-in-out`}>
+        <SunIcon className={`w-5 h-5 ${activeTheme === 'light'? 'fill-white dark:fill-white' : 'fill-[#B2ABAB] dark:fill-white/50 group-hover:fill-[#949191] group-hover:dark:fill-[#dad5d5]'} transition-all duration-200 ease-in-out`} />
         </button>
     </div>
   )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom"
-import { ChartIcon, DiscountIcon, HomeIcon, InfoIcon, LogoIcon, PageIdentifierIcon, ProfileIcon, TrendsIcon } from "../assets/icons"
+import { ArrowRightIcon, ChartIcon, DiscountIcon, HomeIcon, InfoIcon, LogOutIcon, LogoIcon, PageIdentifierIcon, ProfileIcon, SettingsIcon, TrendsIcon } from "../assets/icons"
 import DarkModeToggleButton from "./DarkModeToggleButton";
 
 const SideBar = () => {
@@ -9,52 +9,65 @@ const SideBar = () => {
   return (
     <aside className="py-5 flex flex-col items-center gap-5 bg-[#F7F8FA] border-r border-r-[#EBECF2] h-full">
       <Link to='/' aria-label="Logo(Link to home)" className="flex items-center justify-center">
-        <LogoIcon />
+        <LogoIcon className={'size-8 xl:size-10'} />
       </Link>
 
       <nav className="flex flex-col items-center justify-center gap-1">
         <Link to='/' aria-label="Link to home page" className="flex items-center justify-between group">
-          <button className="size-10 flex items-center justify-center mx-2.5" >
+          <button className="size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5" >
             <HomeIcon className={`${location.pathname === '/' ? 'fill-[#0D062D]' : 'fill-[#B2ABAB]'} group-hover:fill-[#0D062D] transition-all duration-200 ease-in-out`} />
           </button>
           <PageIdentifierIcon className={ `${location.pathname === '/' ? 'visible' : 'invisible'}`} />
         </Link>
 
         <Link to='trends' aria-label="Link to trends" className="flex items-center justify-between group">
-          <button className="size-10 flex items-center justify-center mx-2.5">
+          <button className="size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5">
             <TrendsIcon className={`${location.pathname === '/trends' ? 'stroke-[#0D062D]' : 'stroke-[#B2ABAB]'} group-hover:stroke-[#0D062D] transition-all duration-200 ease-in-out`} />
           </button>
           <PageIdentifierIcon className={ location.pathname === '/trends' ? 'visible' : 'invisible'} />
         </Link>
 
         <Link to='profile' aria-label="Link to profile page" className="flex items-center justify-between group">
-          <button className="size-10 flex items-center justify-center mx-2.5">
+          <button className="size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5">
             <ProfileIcon className={`${location.pathname === '/profile' ? 'stroke-[#0D062D]' : 'stroke-[#B2ABAB]'} group-hover:stroke-[#0D062D] transition-all duration-200 ease-in-out`} />
           </button>
           <PageIdentifierIcon className={ location.pathname === '/profile' ? 'visible' : 'invisible'} />
         </Link>
         <Link to='chart' aria-label="Link to chart page" className="flex items-center justify-between group">
-          <button className="size-10 flex items-center justify-center mx-2.5">
+          <button className="size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5">
           <ChartIcon className={`${location.pathname === '/chart' ? 'stroke-[#0D062D]' : 'stroke-[#B2ABAB]'} group-hover:stroke-[#0D062D] transition-all duration-200 ease-in-out`} />
           </button>
           <PageIdentifierIcon className={ location.pathname === '/chart' ? 'visible' : 'invisible'} />
         </Link>
 
         <Link to='discount' aria-label="Link to discount page" className="flex items-center justify-between group">
-          <button className="size-10 flex items-center justify-center mx-2.5">
+          <button className="size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5">
             <DiscountIcon className={`${location.pathname === '/discount' ? 'stroke-[#0D062D]' : 'stroke-[#B2ABAB]'} group-hover:stroke-[#0D062D] transition-all duration-200 ease-in-out`} />
           </button>
           <PageIdentifierIcon className={ location.pathname === '/discount' ? 'visible' : 'invisible'} />
         </Link>
 
         <Link to='info' aria-label="Link to info page" className="flex items-center justify-between group">
-          <button className="size-10 flex items-center justify-center mx-2.5">
+          <button className="size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5">
             <InfoIcon className={`${location.pathname === '/info' ? 'stroke-[#0D062D]' : 'stroke-[#B2ABAB]'} group-hover:stroke-[#0D062D] transition-all duration-200 ease-in-out`} />
           </button>
           <PageIdentifierIcon  className={ location.pathname === '/info' ? 'visible' : 'invisible'}/>
         </Link>
       </nav>
+
       <DarkModeToggleButton />
+
+      <div className="flex flex-col gap-1 items-center transition-all duration-200 ease-in-out">
+        <button aria-label="Arrow right button" className="group size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5 transition-all duration-200 ease-in-out">
+          <ArrowRightIcon className='stroke-[#B2ABAB] group-hover:stroke-black transition-all duration-200 ease-in-out' />
+        </button>
+        <button aria-label="Settings button, click to open settings" className="group size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5 transition-all duration-200 ease-in-out">
+          <SettingsIcon className='stroke-[#B2ABAB] group-hover:stroke-black transition-all duration-200 ease-in-out' />
+        </button>
+        <button aria-label="Logout button, click to logout" className="group size-9 xl:size-10 flex items-center justify-center mx-2 xl:mx-2.5 transition-all duration-200 ease-in-out">
+          <LogOutIcon className='stroke-[#B2ABAB] group-hover:stroke-black transition-all duration-200 ease-in-out' />
+        </button>
+      </div>
     </aside>
   )
 }
