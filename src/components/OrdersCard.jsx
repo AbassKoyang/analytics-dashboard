bf91be7d85d68f628ba2e2b6013c7f3f0455cd23
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-05 06:37:14 +0930

    add: table to orderscard

--- src/components/OrdersCard.jsx
+++ src/components/OrdersCard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
 const OrdersCard = () => {
   return (
-    <div>OrdersCard</div>
+    <div className="w-full p-4 bg-white dark:bg-black border border-[#EDF2F7] dark:border-white/35 rounded-xl col-span-3">
+        <div className="w-full flex justify-between items-center">
+            <h3 className='font-bold text-lg text-[#26282C] dark:text-white'>Last Orders</h3>
+            <button className="p-0 bg-transparent border-0 outline-0 text-[16px] text-[#34CAA5] font-medium">See all</button>
+        </div>
+        <table>
+        <tr>
+        <th></th>
+        <th>January</th>
+        <th>February</th>
+        <th>March</th>
+    </tr>
+    <tr>
+        <th>Week 1</th>
+        <td>10</td>
+        <td>15</td>
+        <td>12</td>
+    </tr>
+    <tr>
+        <th>Week 2</th>
+        <td>8</td>
+        <td>13</td>
+        <td>11</td>
+    </tr>
+    <tr>
+        <th>Week 3</th>
+        <td>9</td>
+        <td>14</td>
+        <td>10</td>
+    </tr>
+</table>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,11 @@ import totalIncomeGraph from  '../assets/totalincomegraph.png'
 import averageSalesGraph from  '../assets/averagesalesgraph.png'
 import { BoxIcon, CartIcon, CoinIcon, RefundIcon, TrendDownIcon, TrendUpIcon } from "../assets/icons"
 import ChartCard from "../components/ChartCard"
+import OrdersCard from "../components/OrdersCard"
 
 const Home = () => {
   return (
+    <>
     <section className="w-full grid grid-cols-5 gap-3 bg-[#FAFAFA] dark:bg-[#1b1b1b] p-5">
       <ChartCard />
       <div className="w-full grid grid-cols-2 col-span-5 gap-3 lg:col-span-2 self-start">
@@ -17,6 +19,11 @@ const Home = () => {
       <StatsCard graph={totalIncomeGraph} icon={<CoinIcon />} heading='Total Income' value='$350.000' percentage='23.5%' trendIcon={<TrendUpIcon />} text='vs. previous month' textColor='text-[#34CAA5]' backGroundColor='bg-[#34caa43a]'  />
       </div>
     </section>
+
+    <section className="w-full grid grid-cols-5 gap-3 bg-[#FAFAFA] dark:bg-[#1b1b1b] p-5 mt-5">
+      <OrdersCard />
+    </section>
+    </>
   )
 }
 
