3905d5504aaa10e9449380f7ed2005fd7a3ed791
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-09 06:48:50 +0930

    add: title to svgs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
     <section className="w-full max-w-[1400px] grid grid-cols-5 gap-5 bg-[#FAFAFA] dark:bg-[#1b1b1b]">
       <ChartCard />
       <div className="w-full grid grid-cols-2 col-span-5 gap-3 lg:col-span-2 self-start">
-      <StatsCard graph={totalOrderGraph} icon={<RefundIcon />} heading='Total Order' value='350' percentage='23.5%' trendIcon={<TrendUpIcon />} text='vs. previous month' textColor='text-[#34CAA5]' backGroundColor='bg-[#34caa43a]'  />
-      <StatsCard graph={Graph} icon={<BoxIcon />} heading='Total refund' value='270' percentage='23.5%' trendIcon={<TrendDownIcon />} text='vs. previous month' textColor='text-[#ED544E]' backGroundColor='bg-[#ed534e31]'  />
+      <StatsCard graph={totalOrderGraph} icon={<BoxIcon />} heading='Total Order' value='350' percentage='23.5%' trendIcon={<TrendUpIcon />} text='vs. previous month' textColor='text-[#34CAA5]' backGroundColor='bg-[#34caa43a]'  />
+      <StatsCard graph={Graph} icon={<RefundIcon/>} heading='Total refund' value='270' percentage='23.5%' trendIcon={<TrendDownIcon />} text='vs. previous month' textColor='text-[#ED544E]' backGroundColor='bg-[#ed534e31]'  />
       <StatsCard graph={averageSalesGraph} icon={<CartIcon />} heading='Average Sales' value='1567' percentage='23.5%' trendIcon={<TrendDownIcon />} text='vs. previous month' textColor='text-[#ED544E]' backGroundColor='bg-[#ed534e31]'  />
       <StatsCard graph={totalIncomeGraph} icon={<CoinIcon />} heading='Total Income' value='$350.000' percentage='23.5%' trendIcon={<TrendUpIcon />} text='vs. previous month' textColor='text-[#34CAA5]' backGroundColor='bg-[#34caa43a]'  />
       </div>
